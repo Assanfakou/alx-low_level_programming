4e3c55d64debf63fb064daee201231f8c077edd1
Assan kou <fakouhasan@gmail.com>
2024-06-04 15:15:20 +0100

    finelly

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,53 +4,45 @@ void print_times_table(int n)
 {
 	int a, b;
 
-	if(n < 15)
+	if(n > 15)
+		return;
+	a = 0;
+	while(a <= n)
 	{
-		a = 0;
-		while(a <= n)
+		b = 0;
+		while(b <= n)
 		{
-			b = 0;
-			while(b <= n)
+			if(b == 0)
+				_putchar((a * b) + '0');
+			if(a * b <= 9)
 			{
-				if(b == 0)
-				{
-					_putchar(a * b + '0');
-				}
-				if(a * b <= 9)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-					_putchar(32);
-				}
-				else if(a * b > 9 && a * b < 99)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-				}
-				if(a * b > 99)
-				{
-					_putchar(44);
-					_putchar(32);
-				}
-				if(a * b > 99 && a * b < 145)
-				{
-					_putchar((a * b / 100) % 10 + '0');
-				}
-				if(a * b > 9 && a * b < 99)
-				{
-					_putchar((a * b) / 10 + '0');
-					_putchar((a * b) % 10 + '0');
-				}
-				else
-					_putchar(a * b + '0');
-				
-				b++;
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(32);
+				_putchar(a * b + '0');
 			}
-			_putchar('\n');
-			a++;
+			else if(a * b > 9 && a * b <= 99)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar((a * b) / 10 + '0');
+				_putchar((a * b) % 10 + '0');
+			}
+			else if(a * b >= 99)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar((a * b ) / 100 + '0');
+				_putchar(((a * b) / 100) % 10 + '0');
+				_putchar((a * b) % 10 + '0');
+				
+			}	
+			b++;
 		}
-	}	
+		_putchar('\n');
+		a++;
+	}
 	return;
 }

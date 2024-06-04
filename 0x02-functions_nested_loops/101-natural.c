7a279dd3628306759c508dcf559de581ecca9d05
Assan kou <fakouhasan@gmail.com>
2024-06-05 00:51:13 +0100

    cheacking

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* int add(int a)
+{
+	if(a > 0)
+		return a +;
+}*/
+
+int main ()
+{
+	int a, total, mul;
+	
+	a = 0;
+	while(a < 1024)
+	{
+		if(a % 3 == 0 || a % 5 == 0)
+		{
+			a += total;
+			printf("%d ", total);
+		}
+		a++;
+	}
+	printf("\n");
+	return (0);
+}

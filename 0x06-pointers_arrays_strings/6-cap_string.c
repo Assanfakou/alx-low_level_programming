d1cec5517b5b7e7eb3a602dfe6bde5cce0a53889
Assan kou <fakouhasan@gmail.com>
2024-08-03 10:24:44 +0100

    5 push

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * str_to_lower - lowercases eny word that was capitalised
- *
- * @str: the string
- *
- * Return: Nothing.
- */
-/*void str_to_lower(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		i++;
-	}
-}*/
-
-/**
  * cap_string - capitalaize the first charachter in any word
  *
  * @st: the string
@@ -32,14 +12,11 @@ char *cap_string(char *st)
 {
 	int i;
 
-/*	str_to_lower(st);*/
-
+	i = 0;
+	if (st[0] >= 'a' && st[0] <= 'z')
+		st[0] -= 32;
 	while (st[i])
 	{
-/*		if (st[0] >= 'a' && st[0] <= 'z')
-		{
-			st[0] -= 32;
-		}*/
 		if (st[i] == ' ' && (st[i + 1] >= 'a' && st[i + 1] <= 'z'))
 		{
 			st[i + 1] -= 32;
@@ -49,9 +26,13 @@ char *cap_string(char *st)
 			st[i + 1] -= 32;
 		}
 		else if (st[i] == '\n' && (st[i + 1] >= 'a' && st[i + 1] <= 'z'))
+		{
 			st[i + 1] -= 32;
+		}
 		else if (st[i] == '.' && (st[i + 1] >= 'a' && st[i + 1] <= 'z'))
+		{
 			st[i + 1] -= 32;
+		}
 		i++;
 	}
 	return (st);

7c63ae9f19348511f02ddcfd0425a4e49facf099
Assan kou <fakouhasan@gmail.com>
2024-08-03 09:58:12 +0100

    Cappitalise hell

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * str_to_lower - lowercases eny word that was capitalised
+ *
+ * @str: the string
+ *
+ * Return: Nothing.
+ */
+void str_to_lower(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+}
+
+/**
  * cap_string - capitalaize the first charachter in any word
  *
  * @st: the string
@@ -12,9 +32,15 @@ char *cap_string(char *st)
 {
 	int i;
 
+	str_to_lower(st);
+
 	while (st[i])
 	{
-		if (st[i] == ' ' && (st[i + 1] >= 'a' && st[i + 1] <= 'z'))
+		if (st[0] >= 'a' && st[0] <= 'z')
+		{
+			st[0] -= 32;
+		}
+		else if (st[i] == ' ' && (st[i + 1] >= 'a' && st[i + 1] <= 'z'))
 		{
 			st[i + 1] -= 32;
 		}

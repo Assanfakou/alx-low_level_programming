db993f5580b7a996ec4e83d153f6ac6bf8fc56f5
Assan kou <fakouhasan@gmail.com>
2024-08-03 10:11:38 +0100

    4 again

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
  *
  * Return: Nothing.
  */
-void str_to_lower(char *str)
+/*void str_to_lower(char *str)
 {
 	int i;
 
@@ -18,7 +18,7 @@ void str_to_lower(char *str)
 			str[i] += 32;
 		i++;
 	}
-}
+}*/
 
 /**
  * cap_string - capitalaize the first charachter in any word
@@ -32,15 +32,15 @@ char *cap_string(char *st)
 {
 	int i;
 
-	str_to_lower(st);
+/*	str_to_lower(st);*/
 
 	while (st[i])
 	{
-		if (st[0] >= 'a' && st[0] <= 'z')
+/*		if (st[0] >= 'a' && st[0] <= 'z')
 		{
 			st[0] -= 32;
-		}
-		else if (st[i] == ' ' && (st[i + 1] >= 'a' && st[i + 1] <= 'z'))
+		}*/
+		if (st[i] == ' ' && (st[i + 1] >= 'a' && st[i + 1] <= 'z'))
 		{
 			st[i + 1] -= 32;
 		}

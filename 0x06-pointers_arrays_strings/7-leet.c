4a0c5aae5137dcef9ed689ff099fa6b810e0f236
Assan kou <fakouhasan@gmail.com>
2024-08-13 21:54:07 +0200

    the magic task

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - changes the string to chefred code.
+ *
+ * @s: the string that will be chifred.
+ *
+ * Return: s.
+ */
+
+char *leet(char *s)
+{
+	char le[] = "A4a4E3e3T7t7L1l1O0o0";
+	int i = 0;
+	int b;
+
+	while (s[i])
+	*{
+	b = 0;
+		while (le[b])
+		{
+			if (s[i] == le[b])
+			{
+				s[i] = le[b + 1];
+				break;
+			}
+			b++;
+		}
+		i++;
+	}
+	return (s);
+}

e6b45df95525d87ea6ffbb73881d9a892e99ade1
Assan kou <fakouhasan@gmail.com>
2024-08-13 02:21:04 +0200

    print table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,20 @@
+#include <unistd.h>
 #include "main.h"
-
+/**
+ * print_times_table - print table of square.
+ *
+ * @n: the number of squqre
+ *
+ * Return: Always 0.
+ */
 void print_times_table(int n)
 {
 	int a, b;
 
 	if (n > 15)
+	{
 		return;
+	}
 	a = 0;
 	while (a <= n)
 	{
@@ -13,28 +22,24 @@ void print_times_table(int n)
 		while (b <= n)
 		{
 			if (b == 0)
-				_putchar((a * b) + '0');
-			if (a * b <= 9)
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
-				_putchar(32);
-				_putchar(a * b + '0');
+				_putchar('0');
+			}
+			if (b > 0 && a * b <= 9)
+			{
+				write(1, ",   ", 4);
+				_putchar((a * b) + '0');
 			}
 			else if (a * b > 9 && a * b <= 99)
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
+				write(1, ",  ", 3);
 				_putchar((a * b) / 10 + '0');
 				_putchar((a * b) % 10 + '0');
 			}
 			else if (a * b >= 99)
 			{
-				_putchar(44);
-				_putchar(32);
-				_putchar((a * b ) / 100 + '0');
+				write(1, ", ", 2);
+				_putchar((a * b) / 100 + '0');
 				_putchar(((a * b) / 100) % 10 + '0');
 				_putchar((a * b) % 10 + '0');
 			}

078264ea21e8cc2c134f0aba3126a4d2af8592da
Assan kou <fakouhasan@gmail.com>
2024-08-13 16:46:05 +0200

    putnbr_recursion

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,5 @@
 #include "main.h"
 
-/*
-*void print_number(int n)
-*{
-*	unsigned int nb;
-*
-*	if (n < 0)
-*	{
-*		_putchar('-');
-*		nb = -n;
-*	}
-*	else
-*		nb = n;
-*	if (nb > 9)
-*		print_number(nb / 10);
-*	_putchar((nb % 10) + '0');
-}*
-*/
-
 /**
  * print_number - print number in ulterrative
  *
@@ -25,29 +7,52 @@
  *
  * Return: Always 0.
  */
+
 void print_number(int n)
 {
 	unsigned int nb;
-	long int div = 1;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		nb = -n;
 	}
-	else if (n == 0)
-		_putchar('0');
 	else
 		nb = n;
-	while (nb / div > 0)
-	{
-		div = div * 10;
-	}
-	div = div / 10;
-	while (div > 0)
-	{
-		_putchar((nb / div) + '0');
-		nb = nb % div;
-		div = div / 10;
-	}
+	if (nb > 9)
+		print_number(nb / 10);
+	_putchar((nb % 10) + '0');
 }
+/**
+ * print_number - print number in ulterrative
+ *
+ * @n: number that will be recursseved
+ *
+ * Return: Always 0.
+*void print_number(int n)
+*{
+*	unsigned int nb;
+*	long int div = 1;
+*
+*	if (n < 0)
+*	{
+*		_putchar('-');
+*		nb = -n;
+*	}
+*	else if (n == 0)
+*		_putchar('0');
+*	else
+*		nb = n;
+*	while (nb / div > 0)
+*	{
+*		div = div * 10;
+*	}
+*	div = div / 10;
+*	while (div > 0)
+*	{
+*		_putchar((nb / div) + '0');
+*		nb = nb % div;
+*		div = div / 10;
+*	}
+*}
+*/
